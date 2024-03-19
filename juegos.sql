777a77c27166ba0396e98c6193bdd670e5359719
MicaelaLopez25 <mica28368@gmail.com>
2024-03-19 17:12:15 -0300

    me quede hasta donde que usar para agregar todo

--- juegos.sql
+++ juegos.sql
@@ -4,3 +4,4 @@ select * FROM inhabitant where job = 'weaponsmith' and state = 'friendly'
 select * FROM inhabitant where job LIKE '%smith' and state = 'friendly'
 SELECT personid from inhabitant where name = 'Stranger'
 SELECT gold from inhabitant where gold = '0'
+SELECT owner from item where owner is null
